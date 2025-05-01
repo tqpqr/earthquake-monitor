573c28bafda8225812d5ad50715e566ea249f0aa
Aleksandr Motin <a.v.motin@gmal.com>
2025-05-01 18:23:21 +0300

    Crash fix - minutes and magnitude are set in config

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
 
 # Environment
-ENVIRONMENT = os.getenv("ENVIRONMENT")
+ENVIRONMENT = os.getenv("ENVIRONMENT":"production")
 
 # Earthquake monitoring settings
-UPDATE_FREQUENCY_MINUTES = int(os.getenv("UPDATE_FREQUENCY_MINUTES"))  # Частота проверки (минуты)
-MAGNITUDE_THRESHOLD = float(os.getenv("MAGNITUDE_THRESHOLD"))  # Порог магнитуды для публикации
+UPDATE_FREQUENCY_MINUTES = int(os.getenv("UPDATE_FREQUENCY_MINUTES":5))  # Частота проверки (минуты)
+MAGNITUDE_THRESHOLD = float(os.getenv("MAGNITUDE_THRESHOLD":4))  # Порог магнитуды для публикации
 USGS_API_URL = os.getenv("USGS_API_URL", "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson")
